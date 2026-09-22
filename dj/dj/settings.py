d829224175d6804397afa9bce822529509f4036b
maoyed <qwermoaid17@gmail.com>
2026-09-22 09:16:26 +0200

    Add gunicorn for production deployment

--- dj/dj/settings.py
+++ dj/dj/settings.py
@@ -24,12 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#%(^z2=fwqlgvcxamxyukdn@!!vthbi&q5c)(g7j!nu)@iafes'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django_secret_key_placeholder')
 
 
 # Application definition
